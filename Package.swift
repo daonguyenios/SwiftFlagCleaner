e1596dce71653b1719ee6d5af45f7053cfd35644
daniel.nguyen <daniel.nguyen@linecorp.com>
2025-05-07 00:24:56 +0700

    Setup package tool

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "FlagCleaner",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "flagcleaner", targets: ["FlagCleaner"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
+    ],
+    targets: [
+        .executableTarget(name: "FlagCleaner", dependencies: [
+            .product(name: "SwiftSyntax", package: "swift-syntax"),
+            .product(name: "SwiftParser", package: "swift-syntax"),
+            .product(name: "ArgumentParser", package: "swift-argument-parser")
+        ]),
+        .testTarget(name: "FlagCleanerTests", dependencies: ["FlagCleaner"])
+    ]
+)
