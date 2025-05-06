ebb04e1a84e150d6363dbe6af72781251d798fcf
daniel.nguyen <daniel.nguyen@linecorp.com>
2025-05-07 01:01:37 +0700

    Update version of dependencies

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .executable(name: "flagcleaner", targets: ["FlagCleaner"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
     ],
     targets: [
         .executableTarget(name: "FlagCleaner", dependencies: [
